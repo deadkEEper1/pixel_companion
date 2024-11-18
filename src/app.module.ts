bf70b4be8a58c95ce7e9f7c8ecddb8770fae01e3
deadkEEper1 <deadkEEper1@gmail.com>
2024-11-18 18:29:42 -0200

    Db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,22 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppResolver} from './app.resolver';
 import { DungeonsModule } from "./dungeons/dungeons.module";
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql', // Save schema to schema.gql in the root directory
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'Jura',
+      password: 'admin',
+      database: 'pixel_companion',
+      logging: true
     }),
     DungeonsModule,
   ],
