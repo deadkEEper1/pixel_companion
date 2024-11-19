ca24abc52ef1d09bc06452def08e49a90addc6c8
deadkEEper1 <deadkEEper1@gmail.com>
2024-11-19 12:39:43 -0200

    Remove resolver we do not need for now

--- src/level-item/level-item.resolver.ts
+++ src/level-item/level-item.resolver.ts
@@ -1,8 +1,7 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { LevelItemService } from './level-item.service';
 import { LevelItem } from './entities/level-item.entity';
 import { CreateLevelItemInput } from './dto/create-level-item.input';
-import { UpdateLevelItemInput } from './dto/update-level-item.input';
 
 @Resolver(() => LevelItem)
 export class LevelItemResolver {
@@ -11,25 +10,5 @@ export class LevelItemResolver {
   @Mutation(() => LevelItem)
   createLevelItem(@Args('createLevelItemInput') createLevelItemInput: CreateLevelItemInput) {
     return this.levelItemService.create(createLevelItemInput);
-  }
-
-  @Query(() => [LevelItem], { name: 'levelItem' })
-  findAll() {
-    return this.levelItemService.findAll();
-  }
-
-  @Query(() => LevelItem, { name: 'levelItem' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.levelItemService.findOne(id);
-  }
-
-  @Mutation(() => LevelItem)
-  updateLevelItem(@Args('updateLevelItemInput') updateLevelItemInput: UpdateLevelItemInput) {
-    return this.levelItemService.update(updateLevelItemInput.id, updateLevelItemInput);
-  }
-
-  @Mutation(() => LevelItem)
-  removeLevelItem(@Args('id', { type: () => Int }) id: number) {
-    return this.levelItemService.remove(id);
   }
 }
